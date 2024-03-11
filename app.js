fe91fc25544caf67db49b0e5f477d73bf068cf5e
Kautilya Tiwari <160465716+Kautilyaprectice@users.noreply.github.com>
2024-03-11 22:19:44 +0530

    app.js

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-const path = require('path');
-
 const express = require('express');
-const fs = require('fs');
-
-const loginRoutes = require('./Routes/login');
-const messageRoutes = require('./Routes/message');
 
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const loginRoute = require('./Routes/login');
+const messageRoute = require('./Routes/message');
+const contactusRoute = require('./Routes/contactus');
+const successRoute = require('./Routes/success');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(loginRoutes);
-app.use(messageRoutes);
-
-app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname ,'views' , '404.html');
-})
+app.use(loginRoute);
+app.use(messageRoute);
+app.use(contactusRoute);
+app.use(successRoute);
 
 app.listen(3000); 
-
