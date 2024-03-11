6e8062aa21d0060ae6a294e1c16205ec475bd738
Kautilya Tiwari <160465716+Kautilyaprectice@users.noreply.github.com>
2024-03-11 23:58:38 +0530

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const bodyParser = require('body-parser');
@@ -8,12 +10,15 @@ const loginRoute = require('./Routes/login');
 const messageRoute = require('./Routes/message');
 const contactusRoute = require('./Routes/contactus');
 const successRoute = require('./Routes/success');
+const pagenotfound = require('./Routes/404')
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(loginRoute);
 app.use(messageRoute);
 app.use(contactusRoute);
 app.use(successRoute);
+app.use(pagenotfound);
 
 app.listen(3000); 
